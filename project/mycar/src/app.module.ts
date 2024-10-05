24286c82f29a67f5d6bf26c844e7b4c5d75eaa4e
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-10-05 13:50:31 +0300

    finished the db connection and create entity

--- project/mycar/src/app.module.ts
+++ project/mycar/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+
 
 @Module({
-  imports: [],
+  imports: [
+  ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        synchronize: true
+      })
+    }), 
+  ReportsModule,
+  UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
