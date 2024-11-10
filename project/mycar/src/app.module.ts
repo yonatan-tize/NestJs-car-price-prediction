ea31810234511f4e5ce2575e94a1a3f0c57c4ec9
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-11-10 12:06:40 +0300

    finished testing user

--- project/mycar/src/app.module.ts
+++ project/mycar/src/app.module.ts
@@ -11,20 +11,20 @@ import { join } from 'path';
 @Module({
   imports: [
   ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) =>({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get("DB_USERNAME"),
-        password: configService.get("DB_PASSWORD"),
-        database: configService.get("DB_NAME"),
-        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-        synchronize: true
-      })
-    }), 
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) =>({
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: +configService.get('DB_PORT'),
+      username: configService.get("DB_USERNAME"),
+      password: configService.get("DB_PASSWORD"),
+      database: configService.get("DB_NAME"),
+      entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+      synchronize: true
+    })
+  }), 
   ReportsModule,
   UsersModule],
   controllers: [AppController],
--- project/mycar/src/main.ts
+++ project/mycar/src/main.ts
@@ -1,20 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session')
-
+import { setupApp } from './setup-app';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['my_session_key']
-    })
-  )
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
+  setupApp(app)
   await app.listen(3000);
 }
 bootstrap();
